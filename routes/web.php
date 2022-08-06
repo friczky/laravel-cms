895b856badfa85eca5302883c57db2c7d4831885
Fadilah Riczky <friczky@gmail.com>
2022-08-06 20:53:15 +0700

    :zap: update route Auth

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ArticlesController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
